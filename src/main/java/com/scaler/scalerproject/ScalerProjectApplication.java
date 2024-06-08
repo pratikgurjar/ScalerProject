f6ee1a2cd81035be4bfde1a08e018f525b7a4f13
pg06pg <pratikgurjar66@gmail.com>
2024-06-09 02:02:55 +0530

    Initial Commit

--- src/main/java/com/scaler/scalerproject/ScalerProjectApplication.java
+++ src/main/java/com/scaler/scalerproject/ScalerProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoProjectApplication {
+public class ScalerProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoProjectApplication.class, args);
+        SpringApplication.run(ScalerProjectApplication.class, args);
     }
 
 }
